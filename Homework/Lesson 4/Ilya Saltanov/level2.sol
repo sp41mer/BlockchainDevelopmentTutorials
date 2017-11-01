52f8287815cd1bdaf623e038a3bd1385988968a4
Ilya Saltanov <isaltanov@gmail.com>
2017-11-01 18:43:39 +0300

    Update level2.sol

--- Homework/Lesson 4/Ilya Saltanov/level2.sol
+++ Homework/Lesson 4/Ilya Saltanov/level2.sol
@@ -1,11 +1,20 @@
-pragma solidity ^0.4.11;
-
-
-//Объявляем контракт
-contract Lesson4Level2App {
-
-  function Lesson4Level2App {
+contract HWL4L2Contract {
+	uint public costOfMagic;
+	uint public magicNumber;
+	
+  function HWL4L2Contract () public {
+	    magicNumber = 0;
+	    costOfMagic = 1 * 1 ether;
+	}
+	
+	function setMagicNumber (uint _magicNumber) internal{
+	    magicNumber = _magicNumber;
+	}
     
+  function MagicForEther(uint _magicNumber ) public payable {
+	
+      if (msg.value == costOfMagic ) {
+         setMagicNumber(_magicNumber);
+      }
   }
-
 }
