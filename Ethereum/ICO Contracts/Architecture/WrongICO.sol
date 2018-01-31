81dda7226f55af21c66ef6d2bd8e73989fbaa2af
sp41mer <sp41mer@yahoo.com>
2018-01-30 20:57:43 -0800

    wrong ico

--- Ethereum/ICO Contracts/Architecture/WrongICO.sol
+++ Ethereum/ICO Contracts/Architecture/WrongICO.sol
@@ -1,7 +1,105 @@
-pragma solidity ^0.4.0;
+// Так делать не нужно :)
 
-contract WrongICO {
-    function WrongICO(){
+// Указываем версию для компилятора
+pragma solidity ^0.4.11;
 
+
+// Инициализация контракта
+contract MyFirstERC20Coin {
+
+    // Объявляем переменную в которой будет название токена
+    string public name;
+    // Объявляем переменную в которой будет символ токена
+    string public symbol;
+    // Объявляем переменную в которой будет число нулей токена
+    uint8 public decimals;
+
+    // Добавляем переменную для стоимости одного токена
+
+    // Объявляем переменную в которой будет храниться общее число токенов
+    uint256 public totalSupply;
+
+    // Объявляем маппинг для хранения балансов пользователей
+    mapping (address => uint256) public balanceOf;
+    // Объявляем маппинг для хранения одобренных транзакций
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    // Объявляем эвент для логгирования события перевода токенов
+    event Transfer(address from, address to, uint256 value);
+    // Объявляем эвент для логгирования события одобрения перевода токенов
+    event Approval(address from, address to, uint256 value);
+
+    // Функция инициализации контракта
+    function MyFirstERC20Coin(){
+        // Указываем число нулей
+        decimals = 8;
+        // Объявляем общее число токенов, которое будет создано при инициализации
+        totalSupply = 10000000 * (10 ** uint256(decimals));
+        // 10000000 * (10^decimals)
+
+        // "Отправляем" все токены на баланс того, кто инициализировал создание контракта токена
+        balanceOf[this] = totalSupply;
+
+        // Указываем стоимость токена
+
+        // Указываем название токена
+        name = "MyFirstERC20Coin";
+        // Указываем символ токена
+        symbol = "MFE";
     }
+
+    // Внутренняя функция для перевода токенов
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != 0x0);
+        // Проверка на пустой адрес
+        require(balanceOf[_from] >= _value);
+        // Проверка того, что отправителю хватает токенов для перевода
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+
+        balanceOf[_to] += _value;
+        // Токены списываются у отправителя
+        balanceOf[_from] -= _value;
+        // Токены прибавляются получателю
+
+        Transfer(_from, _to, _value);
+        // Перевод токенов
+    }
+
+    // Функция для перевода токенов
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+        // Вызов внутренней функции перевода
+    }
+
+    // Функция для перевода "одобренных" токенов
+    function transferFrom(address _from, address _to, uint256 _value) public {
+        // Проверка, что токены были выделены аккаунтом _from для аккаунта _to
+        require(_value <= allowance[_from][_to]);
+        allowance[_from][_to] -= _value;
+        // Отправка токенов
+        _transfer(_from, _to, _value);
+    }
+
+    // Функция для "одобрения" перевода токенов
+    function approve(address _to, uint256 _value){
+        allowance[msg.sender][_to] = _value;
+        // Вызов эвента для логгирования события одобрения перевода токенов
+        Approval(msg.sender, _to, _value);
+    }
+
+    // Функция для отправки эфиров на контракт
+
+        // Выполняем внутреннюю функцию контракта
+
+
+    // Функция для отправки эфиров на контракт (вызываемая)
+
+
+
+    // Внутренняя функция покупки
+
+        // Получаем количество возможных для покупки токенов по курсу
+
+        // Вызываем внутреннюю функцию перевода токенов
+
 }
